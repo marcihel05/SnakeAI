10decea1460c9d52343bc30a46851a9f0b0a4b8f
marcihel05 <helena.marcius05@gmail.com>
2021-01-12 22:11:26 +0100

    main program

---
+++ world.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tenserflow as tf
+from tf import keras
+from keras import Sequential
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import random
+import time
+from population import Population
+from snake import Snake
+from brain import Brain
+
+FPS = 10
+
+def main():
+    population = Population()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
