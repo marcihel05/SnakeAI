e40fa1ec797f4d34686cb17901f771970a77d262
marcihel05 <helena.marcius05@gmail.com>
2021-01-13 11:58:24 +0100

    Matrix class for brain

---
+++ matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+#import tensorflow as tf
+#from tf import keras
+import keras
+from keras import Sequential
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import random
+import time
+
+class Matrix():
