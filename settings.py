146451724de8d4e1b10ea909a5875a4852c2fa94
marcihel05 <helena.marcius05@gmail.com>
2021-02-03 01:11:28 +0100

    game settings in separate file

---
+++ settings.py
@@ -0,0 +1,32 @@
+NUMBER_OF_SNAKES = 2000 #number of snakes in population
+
+MUTATION_RATE = 0.1
+
+SAVE_GEN = 10 #save brain of best snake after SAVE_GEN generations
+SAVE_SCORE = 100 #save when snake reaches SAVE_SCORE points
+
+INPUT = 24
+HIDDEN1 = 16
+HIDDEN2 = 16
+OUTPUT = 4
+
+#dimensions of screen and game
+WIN_HEIGHT = 950
+WIN_WIDTH = 1700
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+
+
+RECT_DIM = 20 #dimension of snake's part and food
+VELOCITY = 20 #velocity of snake
+
+#used colors
+BLACK = (0,0,0)
+RED = (255, 0, 0)
+WHITE = (255,255,255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0,0,255)
+ORANGE = (255,128,0)
+
+BRAIN_FILE = 'brain'
