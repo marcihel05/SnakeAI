9137c8634376ee305aa79afa43564c47152d319a
marcihel05 <helena.marcius05@gmail.com>
2021-02-03 01:12:10 +0100

    save brain of best snake implemented

--- population.py
+++ population.py
@@ -5,16 +5,15 @@ import pygame
 import random
 import time
 from snake import Snake
+from settings import *
 
 
-NUM_OF_SNAKES = 2000
-MUTATION_RATE = 0.05
 
 class Population:
-    def __init__(self, num = NUM_OF_SNAKES):
-        self.num_of_snakes = num
+    def __init__(self):
+        self.num_of_snakes = NUMBER_OF_SNAKES 
         self.snakes = []
-        self.currentBestSnake = "" #index of current beast snake (highest score)
+        self.currentBestSnake = 0 #index of current beast snake (highest score)
         self.globalBestSnake = "" #index of best snake in this generation (fitness score)
         self.globalBestScore = 0 #best score of this generation
         self.globalFitness = 0 #fitness score of current generation
@@ -24,16 +23,16 @@ class Population:
         for i in range(0, self.num_of_snakes):
             self.snakes.append(Snake())
         
-        self.currentBestSnake = 0
-        self.globalBestSnake = 0
+        #self.currentBestSnake = 0
+        #self.globalBestSnake = 0
 
     def findCurrentBestSnake(self):
         if not self.snakes[self.currentBestSnake].dead:
             maxScore = self.snakes[self.currentBestSnake].score
             maxSnake = self.currentBestSnake
         else:
-            maxScore = 0
-            maxSnake = 0
+            maxScore = -1
+            maxSnake = -1
         i = 0 #index of snake in self.snakes
         for snake in self.snakes:
             if snake.score > maxScore and not snake.dead:
@@ -69,9 +68,9 @@ class Population:
                 maxFitness = snake.fitness
                 maxIndex = i
             i+=1
-        if maxFitness > self.bestGlobalFitness:
+        if maxFitness >= self.bestGlobalFitness:
             self.bestGlobalFitness = maxFitness
-            self.globalBestSnake = maxIndex
+            self.globalBestSnake = self.snakes[maxIndex].clone()#maxIndex
 
 
     def calcFitness(self):
@@ -79,20 +78,22 @@ class Population:
             snake.calcFitness()
 
 
-    def genetic_algorithm(self): ##modify this
+    def genetic_algorithm(self): 
         self.calcFitness() #calculate fitness of each snake
         self.findGlobalBestSnake() #find best snake of this generation
         self.calcGlobalFitness() #calc sum of fitness scores of all snakes in this generation
-        new_snakes = [self.snakes[self.globalBestSnake].clone()]
+        #self.normalizeFitness()
+        #new_snakes = [self.snakes[self.globalBestSnake].clone()]
+        new_snakes = [self.globalBestSnake.clone()]
         for i in range(1, len(self.snakes)): #find new snakes
             parent1 = self.selectParent() #find parents
             parent2 = self.selectParent()
             child = parent1.crossover(parent2) #make new child
-            child.mutate() #mutate child
+            #child.mutate() #mutate child
             new_snakes.append(child)
         self.snakes = new_snakes
         self.currentBestSnake = 0
-        self.globalBestSnake = 0
+        #self.globalBestSnake = 0
         self.genBestScore = 0
     
     def calcGlobalFitness(self):
@@ -101,10 +102,15 @@ class Population:
             fitness+=snake.fitness
         self.globalFitness = fitness
 
+    #def normalizeFitness(self):
+     #   for snake in self.snakes:
+      #      snake.fitness = snake.fitness/self.globalFitness
+
 
     def selectParent(self):
         #random.seed(time.time())
         rand = random.randrange(0, self.globalFitness)
+        #rand = random.uniform(0,1)
         sum = 0
         for snake in self.snakes:
             sum+=snake.fitness
@@ -132,7 +138,7 @@ class Population:
     
     def draw(self, win, toShow):
         if toShow[0] == 1:
-            self.findCurrentBestSnake()
+          #  self.findCurrentBestSnake()
             self.snakes[self.currentBestSnake].draw(win)
         if toShow[0] == 5:
             bestSnakes = self.findCurrentFiveBestSnakes()
@@ -145,6 +151,26 @@ class Population:
         if toShow[1] == 1:
             index = np.argmax(self.snakes[self.currentBestSnake].decision)
             self.snakes[self.currentBestSnake].brain.draw(win,index)
+
+    def saveBestSnake(self): #save brain of best snake
+        id = random.randint(0, 50)
+        name = 'brain' + str(id)
+        f = open(name, 'x')
+        for i in range(0, HIDDEN1):
+            for j in range(0, INPUT+1):
+                f.write('%f\n' % self.globalBestSnake.brain.wih.matrix[i][j])
+
+        for i in range(0, HIDDEN2):
+            for j in range(0, HIDDEN1+1):
+                f.write('%f\n' % self.globalBestSnake.brain.whh.matrix[i][j])
+
+        for i in range(0, OUTPUT):
+            for j in range(0, HIDDEN2+1):
+                f.write('%f\n' % self.globalBestSnake.brain.who.matrix[i][j])
+
+        f.close()
+
+
 
         
 
